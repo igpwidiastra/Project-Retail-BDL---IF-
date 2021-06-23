066091e08674c9fe4994ee9635496981711b2341
I Gede Pageh Widiastra <86338178+igpwidiastra@users.noreply.github.com>
2021-06-23 14:27:18 +0800

    Add files via upload

---
+++ tokopekita.sql
@@ -0,0 +1,322 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 23, 2021 at 07:55 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tokopekita`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `idcart` int(11) NOT NULL,
+  `orderid` varchar(100) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `tglorder` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` varchar(10) NOT NULL DEFAULT 'Cart'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`idcart`, `orderid`, `userid`, `tglorder`, `status`) VALUES
+(10, '15wKVT0nej25Y', 2, '2020-03-16 12:10:42', 'Selesai'),
+(11, '15Swf8Ye0Fm.M', 2, '2020-03-16 12:17:34', 'Cart'),
+(12, '15PzF03ejd8W2', 1, '2020-05-13 02:40:48', 'Confirmed'),
+(13, '16uPeuP5t2AzQ', 3, '2021-06-02 13:24:19', 'Selesai'),
+(14, '16S0B7DEovCqE', 1, '2021-06-04 10:59:33', 'Payment'),
+(15, '16FTBqZ5LI/QE', 4, '2021-06-23 05:50:47', 'Payment');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detailorder`
+--
+
+CREATE TABLE `detailorder` (
+  `detailid` int(11) NOT NULL,
+  `orderid` varchar(100) NOT NULL,
+  `idproduk` int(11) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `detailorder`
+--
+
+INSERT INTO `detailorder` (`detailid`, `orderid`, `idproduk`, `qty`) VALUES
+(15, '16uPeuP5t2AzQ', 4, 1),
+(16, '16S0B7DEovCqE', 4, 2),
+(17, '16S0B7DEovCqE', 5, 2),
+(18, '16FTBqZ5LI/QE', 4, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `idkategori` int(11) NOT NULL,
+  `namakategori` varchar(20) NOT NULL,
+  `tgldibuat` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`idkategori`, `namakategori`, `tgldibuat`) VALUES
+(4, 'Laptop', '2021-06-02 12:54:56'),
+(5, 'Handphone', '2021-06-04 11:54:44'),
+(6, 'Aksesoris', '2021-06-04 11:54:59');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `konfirmasi`
+--
+
+CREATE TABLE `konfirmasi` (
+  `idkonfirmasi` int(11) NOT NULL,
+  `orderid` varchar(100) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `payment` varchar(10) NOT NULL,
+  `namarekening` varchar(25) NOT NULL,
+  `tglbayar` date NOT NULL,
+  `tglsubmit` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `konfirmasi`
+--
+
+INSERT INTO `konfirmasi` (`idkonfirmasi`, `orderid`, `userid`, `payment`, `namarekening`, `tglbayar`, `tglsubmit`) VALUES
+(1, '15PzF03ejd8W2', 1, 'Bank BCA', 'Admin', '2020-05-16', '2020-05-13 02:41:51');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `userid` int(11) NOT NULL,
+  `namalengkap` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `notelp` varchar(15) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `tgljoin` timestamp NOT NULL DEFAULT current_timestamp(),
+  `role` varchar(7) NOT NULL DEFAULT 'Member',
+  `lastlogin` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`userid`, `namalengkap`, `email`, `password`, `notelp`, `alamat`, `tgljoin`, `role`, `lastlogin`) VALUES
+(1, 'Admin', 'admin', '$2y$10$GJVGd4ji3QE8ikTBzNyA0uLQhiGd6MirZeSJV1O6nUpjSVp1eaKzS', '01234567890', 'Indonesia', '2020-03-16 11:31:17', 'Admin', NULL),
+(2, 'Guest', 'guest', '$2y$10$xXEMgj5pMT9EE0QAx3QW8uEn155Je.FHH5SuIATxVheOt0Z4rhK6K', '01234567890', 'Indonesia', '2020-03-16 11:30:40', 'Member', NULL),
+(4, 'Pageh', 'pagehjoss@gmail.com', '$2y$10$74ulLRwDQPSAMSu2TH7UVeVS5qq/NZz1Vd5x88FEVU3VXH9we9GCi', '085792736627', 'Denpasar', '2021-06-23 05:50:08', 'Member', NULL),
+(5, 'Pageh', 'pagehjoss@gmail.com', '$2y$10$MSVmVs2ZCv5Fg3zPQGVKSOBP8ouLWRHS/GJiABkFqmK/ylDi5Fr1G', '085792736627', 'Denpasar', '2021-06-23 05:50:09', 'Member', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `no` int(11) NOT NULL,
+  `metode` varchar(25) NOT NULL,
+  `norek` varchar(25) NOT NULL,
+  `logo` text DEFAULT NULL,
+  `an` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pembayaran`
+--
+
+INSERT INTO `pembayaran` (`no`, `metode`, `norek`, `logo`, `an`) VALUES
+(1, 'Bank BCA', '13131231231', 'images/bca.jpg', 'Tokopekita'),
+(2, 'Bank Mandiri', '943248844843', 'images/mandiri.jpg', 'Tokopekita'),
+(3, 'DANA', '0882313132123', 'images/dana.png', 'Tokopekita');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
+
+CREATE TABLE `produk` (
+  `idproduk` int(11) NOT NULL,
+  `idkategori` int(11) NOT NULL,
+  `namaproduk` varchar(30) NOT NULL,
+  `gambar` varchar(100) NOT NULL,
+  `deskripsi` varchar(200) NOT NULL,
+  `rate` int(11) NOT NULL,
+  `hargabefore` int(11) NOT NULL,
+  `hargaafter` int(11) NOT NULL,
+  `tgldibuat` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`idproduk`, `idkategori`, `namaproduk`, `gambar`, `deskripsi`, `rate`, `hargabefore`, `hargaafter`, `tgldibuat`) VALUES
+(4, 4, 'Latop Series 1', 'produk/16mEiuskfMHf2.jpg', 'Laptop berkualitas murah dan terjamin awet', 4, 8000000, 8700000, '2021-06-02 12:58:00'),
+(5, 4, 'Laptop - Series 2', 'produk/16e4BQcWaVDRc.jpg', 'Laptop berkualitas murah dan terjamin awet', 5, 12000000, 11500000, '2021-06-04 11:56:11'),
+(6, 4, 'Lenovo ideapad 320 14\" (14AST ', 'produk/16yfRrNng6a1o.jpg', 'Kondisi: Baru Berat: 1.000 Gram Kategori: Laptop Consumer Etalase: LAPTOP Lenovo ideapad 320 14\"  Processor AMD A9 Harddisk 1TB Ram 4GB DVD Rom VGA 2GB Win.10 Home Free Kabel data  Laptop ini sudah te', 5, 5000000, 4500000, '2021-06-11 02:58:34'),
+(7, 5, 'Xiaomi Official Poco X3', 'produk/16JxZSFlxkocw.jpg', 'Kondisi: Baru Berat: 500 Gram Kategori: Android OS Etalase: Poco Xiaomi Official Store Garansi Resmi, IMEI Resmi Poco X3 menggunakan Qualcomm®️ Snapdragon™️ 732G 6GB LPDDR4X Hingga 128GB UFS2.1. Skor ', 5, 3200000, 2850000, '2021-06-11 03:05:30'),
+(8, 5, 'Vivo Y91c', 'produk/16ADfZify846U.png', 'Kondisi: Bekas Berat: 500 Gram Kategori: Android OS Etalase: Semua Etalase Selamat datang di Sinar Citangkill ! Selalu ready stock selama iklan ini masih aktif ya kak :) Handphone Second  Grade A : ke', 4, 2000000, 1570000, '2021-06-11 03:07:42');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`idcart`),
+  ADD UNIQUE KEY `orderid` (`orderid`),
+  ADD KEY `orderid_2` (`orderid`);
+
+--
+-- Indexes for table `detailorder`
+--
+ALTER TABLE `detailorder`
+  ADD PRIMARY KEY (`detailid`),
+  ADD KEY `orderid` (`orderid`),
+  ADD KEY `idproduk` (`idproduk`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`idkategori`);
+
+--
+-- Indexes for table `konfirmasi`
+--
+ALTER TABLE `konfirmasi`
+  ADD PRIMARY KEY (`idkonfirmasi`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`no`);
+
+--
+-- Indexes for table `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`idproduk`),
+  ADD KEY `idkategori` (`idkategori`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `idcart` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `detailorder`
+--
+ALTER TABLE `detailorder`
+  MODIFY `detailid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `idkategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `konfirmasi`
+--
+ALTER TABLE `konfirmasi`
+  MODIFY `idkonfirmasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `idproduk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `detailorder`
+--
+ALTER TABLE `detailorder`
+  ADD CONSTRAINT `idproduk` FOREIGN KEY (`idproduk`) REFERENCES `produk` (`idproduk`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `orderid` FOREIGN KEY (`orderid`) REFERENCES `cart` (`orderid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `konfirmasi`
+--
+ALTER TABLE `konfirmasi`
+  ADD CONSTRAINT `userid` FOREIGN KEY (`userid`) REFERENCES `login` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `produk`
+--
+ALTER TABLE `produk`
+  ADD CONSTRAINT `idkategori` FOREIGN KEY (`idkategori`) REFERENCES `kategori` (`idkategori`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
